e38bb97055b80d0395851efc9426c5433d3a2c1a
huangjianlou <huangjianlou@dazhi100.com>
2022-04-13 13:46:09 +0800

    first commit

---
+++ dzyx-school/service/src/main/java/com/dazhi100/school/SchoolApplication.java
@@ -0,0 +1,16 @@
+package com.dazhi100.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: huangjl
+ * @date: 2022/4/13
+ */
+@SpringBootApplication
+public class SchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolApplication.class, args);
+    }
+}
